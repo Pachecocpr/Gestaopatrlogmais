1f139a9a9b983ff66ad1e89044a1b6dc9bd321ca
Pachecocpr <99982916+Pachecocpr@users.noreply.github.com>
2026-02-23 20:12:17 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Inventory Pro", page_icon="📦", layout="wide")
 NOME_DAS_UNIDADES = [
     "CLI CONTAGEM", "CLI BELO HORIZONTE/DR/MG", 
 "CLI TAPERA", "CLI ARO",
-    "CLI UNIVERSITARIO", "CLI DEFENSORIA PÚBLICA", 
+    "CLI UNIVERSITARIO", "CLI DEFENSORIA PUBLICA", 
 "CLI TJ MG", "GELOG MG",
     "CLI INDAIA", "CEDIP", "GELOG MG", 
 "CLI CAIXA"
